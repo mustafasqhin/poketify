5da77cd7e81f2cd36f9c784a6a88c52f66b6a4e6
mustafasqhin <mustafasqhin64@gmail.com>
2024-12-19 09:42:34 +0300

    ....

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
-const path = require('path'); // Eğer ES module kullanıyorsanız: import path from 'path';
-
 export default {
-  entry: './public/scripts/app/app.js', // Giriş dosyanız
-  mode: 'production', // Üretim modu
-  output: {
-    publicPath: '/',
-    path: path.resolve(process.cwd(), 'public/scripts/app/bundle'), // Çıkış dizini
-    filename: 'bundle.js' // Çıkış dosyasının adı
-  },
+	entry: './public/scripts/app/app.js',
+	mode: 'production',
+	output: {
+		publicPath: '/',
+		path: process.cwd() + '/public/scripts/app/bundle',
+		filename: 'bundle.js'
+	},
 };
